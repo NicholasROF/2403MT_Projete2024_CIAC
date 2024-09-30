f72d4ba3632577696b7fc7143f6944766c48a098
NicholasROF <152822714+NicholasROF@users.noreply.github.com>
2024-09-30 08:14:47 -0300

    Atualização do código do site
    Correção de erros e sincronia com firmware

--- Cógidos do site/temp.js
+++ Cógidos do site/temp.js
@@ -1,46 +1,54 @@
-// Conectar ao broker EMQX
-const client = mqtt.connect('wss://broker.emqx.io:8084/mqtt'); // estabelece conexão com o broker no endereço
 
-client.on('connect', function () { // se a conexão com o broker for bem sucedida 
-console.log('Conectado ao broker EMQX'); // exibe console no navegador 
-client.subscribe('Temperatura/Nick', function (err) { // fornece mensagem relacionada a temperatura
-if (!err) { // se não houver erro
-console.log('Inscrito no tópico Temperatura/Nick'); // mensagem de exibição
-}
-});
-});
+// **Conectar ao broker EMQX
+const client1 = mqtt.connect('wss://broker.emqx.io:8084/mqtt'); // estabelece conexão com o broker no endereço
 
-client.on('message', function (topic, message) { // se a mensagem 
-const messagesDiv = document.getElementById('messages'); //
-const newMessage = document.createElement('p'); //
-newMessage.textContent = `Tópico: ${topic} - Mensagem: ${message.toString()}`; //
-messagesDiv.appendChild(newMessage); //
+client1.on('connect', function () {
+    console.log('Conectado ao broker EMQX');
+    client1.subscribe('Temperatura/Nick', function (err) {
+        if (!err) {
+            console.log('Inscrito no tópico Temperatura/Nick');
+        }
+    });
 });
 
-function sendMessage() { //
-   const messageInput = document.getElementById('messageInput'); //
-   const message = messageInput.value; //
-   client.publish('Temperatura/Nick', message); //
-   messageInput.value = ''; //
-}
-function sendMessageTemp() { //
-   const messageInput = document.getElementById('tempInput'); //
-   const message = messageInput.value; //
-   client.publish('Temperatura/Nick', message); //
+client1.on('message', function (topic, message) {
+    const messagesDiv = document.getElementById('messages');
+    const newMessage = document.createElement('p');
+    //const new1Message = document.createElement('p');
+    newMessage.textContent = `Temperatura ${acionamento.toString()}: ${message.toString()}°C`;
+    //new1Message.textContent = `Temperatura de acimonamento: ${message.toString()}°C`;
+
+    messagesDiv.appendChild(newMessage);
+    
+    // Atualizar a célula de análise de dados para o parâmetro Ar Condicionado
+    document.getElementById('mensagemAIR_TEMP').textContent = message.toString()+" °C";
+});
+
+function sendMessage() {
+    const messageInput = document.getElementById('messageInput');
+    const message = messageInput.value;
+
+    client1.publish('CIAC/AIR_TEMP', message);
+    acionamento = "do ar-condcionado"
+
+    // Atualiza a célula de análise de dados para o parâmetro Ar Condicionado
+    document.getElementById('mensagemAIR_TEMP').textContent = message + " °C";
+
+    messageInput.value = ''; // Limpa o campo de entrada
 }
 
-function sendMessageTemp2() { //
-   const messageInput = document.getElementById('temp2Input'); //
-   const message = messageInput.value; //
-   client.publish('Temperatura/Nick', message); //
+function sendMessageTemp() {
+    const messageInput = document.getElementById('tempInput');
+    const message = messageInput.value;
+    acionamento = "de acionamento máxima";
+    console.log("leu");
+    client1.publish('CIAC/AIR_CONT_ON', message);
 }
-               
-    function ligar() {
-       alert("Ar condicionado ligado!");
-           // adicionar qualquer lógica adicional para "ligar" a luz.
-       }
 
-       function desligar() {
-           alert("Arcondicionado desligado !");
-           // adicionar qualquer lógica adicional para "desligar" a luz.
-               }
+function sendMessageTemp2() {
+    const messageInput = document.getElementById('temp2Input');
+    const message = messageInput.value;
+    acionamento = "de acionamento mínima";
+    console.log("leu");
+    client1.publish('CIAC/AIR_CONT_OFF', message);
+}
